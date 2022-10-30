999a590d87bc714a1a268cfcf5fd5c6baab33e3e
sarakara <gulcinsarankara@hotmail.com>
2022-10-30 14:55:54 -0400

    Tree

---
+++ dsa-08-Trees/src/main/java/MyTree.java
@@ -0,0 +1,59 @@
+import javax.crypto.spec.PSource;
+
+public class MyTree {
+    TNode root;
+
+    public MyTree() {
+    }
+    void insert(int value){
+        TNode newNode=new TNode(value);
+        if (root==null) {
+            root=newNode;
+            return;
+        }
+        TNode current =root;
+        while(true){
+            if (value<= current.value){
+
+                if (current.leftChild==null){
+                    // if left is null insert there!!!
+                    current.leftChild=newNode;
+                    break;
+                }
+                //if leftChild is not null branch into left subtree!!
+                current=current.leftChild;
+            }
+            else {
+                if (current.rightChild==null){
+                    // if right is null insert there!!!
+                    current.rightChild=newNode;
+                    break;
+                }
+                //if rightChild is not null branch into right subtree!!
+                current=current.rightChild;
+            }
+        }
+    }
+    // PreOrder Traversal of the tree
+    // Root-Left-right
+    void preOrderTraversal(TNode root){
+        if(root==null) return;
+        System.out.print(root.value +", ");
+        preOrderTraversal(root.leftChild);
+        preOrderTraversal(root.rightChild);
+    }
+    void inOrderTraversal(TNode root){
+        if (root==null) return;
+        inOrderTraversal(root.leftChild);
+        System.out.print(root.value+", ");
+        inOrderTraversal(root.rightChild);
+    }
+    void PostOrderTraversal(TNode root){
+        if (root==null) return;
+        PostOrderTraversal(root.leftChild);
+        PostOrderTraversal(root.rightChild);
+        System.out.print(root.value+", ");
+
+    }
+
+}
